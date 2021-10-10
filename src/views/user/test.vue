c6bd39f3902c0891e81d968f14994d31d90be7c6
zhaochenxi <15182762040@163.com>
2021-10-10 16:25:59 +0800

    1010

--- src/views/user/test.vue
+++ src/views/user/test.vue
@@ -1,156 +1,153 @@
 <template>
-  <el-form :model="ruleForm"
-           :rules="rules"
-           ref="ruleForm"
-           label-width="100px"
-           class="demo-ruleForm">
-    <el-form-item label="活动名称"
-                  prop="name">
-      <el-input v-model="ruleForm.name"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域"
-                  prop="region">
-      <el-select v-model="ruleForm.region"
-                 placeholder="请选择活动区域">
-        <el-option label="区域一"
-                   value="shanghai"></el-option>
-        <el-option label="区域二"
-                   value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item label="活动时间"
-                  required>
-      <el-col :span="11">
-        <el-form-item prop="date1">
-          <el-date-picker type="date"
-                          placeholder="选择日期"
-                          v-model="ruleForm.date1"
-                          style="width: 100%;"></el-date-picker>
-        </el-form-item>
-      </el-col>
-      <el-col class="line"
-              :span="2">-</el-col>
-      <el-col :span="11">
-        <el-form-item prop="date2">
-          <el-time-picker placeholder="选择时间"
-                          v-model="ruleForm.date2"
-                          style="width: 100%;"></el-time-picker>
-        </el-form-item>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="即时配送"
-                  prop="delivery">
-      <el-switch v-model="ruleForm.delivery"></el-switch>
-    </el-form-item>
-    <el-form-item label="活动性质"
-                  prop="type">
-      <el-checkbox-group v-model="ruleForm.type">
-        <el-checkbox label="美食/餐厅线上活动"
-                     name="type"></el-checkbox>
-        <el-checkbox label="地推活动"
-                     name="type"></el-checkbox>
-        <el-checkbox label="线下主题活动"
-                     name="type"></el-checkbox>
-        <el-checkbox label="单纯品牌曝光"
-                     name="type"></el-checkbox>
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="特殊资源"
-                  prop="resource">
-      <el-radio-group v-model="ruleForm.resource">
-        <el-radio label="线上品牌商赞助"></el-radio>
-        <el-radio label="线下场地免费"></el-radio>
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="活动形式"
-                  prop="desc">
-      <el-input type="textarea"
-                v-model="ruleForm.desc"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary"
-                 @click="submitForm('ruleForm')">立即创建</el-button>
-      <el-button @click="resetForm('ruleForm')">重置</el-button>
-    </el-form-item>
-  </el-form>
+
+  <body id="login-page">
+
+    <div class="login-container">
+      <h3 class="login_title">logo</h3>
+      <!-- 登陆切换 -->
+      <div class="TagPage">
+        <el-tabs v-model="activeName"
+                 type="card"
+                 @tab-click="handleClick">
+          <el-tab-pane label="账号登录">
+            <el-form label-position="left"
+                     label-width="0px"
+                     :rules="rules">
+              <el-form-item prop="userAccount">
+                <el-input type="text"
+                          v-model="loginForm.userAccount"
+                          auto-complete="off"
+                          placeholder="账号"
+                          oninput="value=value.replace(/[^\d]/g,'')"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-input type="password"
+                          v-model="loginForm.userPwd"
+                          auto-complete="off"
+                          placeholder="密码"></el-input>
+              </el-form-item>
+              <div class="down-btn">
+                <el-checkbox v-model="loginForm.rememberMe"
+                             label="记住我"></el-checkbox>
+                <a href="http://localhost/user/FindPwd"
+                   class="right-btn">忘记密码？</a>
+              </div>
+              <el-form-item style="width: 100%">
+                <el-button type="primary"
+                           style="width: 100%;  border: none ;margin-top:20px;"
+                           @click="userLogin()">登录</el-button>
+              </el-form-item>
+              <div class="register">
+                还没有账号？<a href="http://localhost/user/register">点击注册</a>
+              </div>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="手机登录">
+
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+
+    </div>
+  </body>
 </template>
+
 <script>
 export default {
+  name: 'Login',
   data () {
     return {
-      ruleForm: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      loginForm: {
+        userAccount: '',
+        userPwd: ''
+        // rememberMe: ''
       },
-      rules: {
-        name: [{
-          required: true,
-          message: '请输入活动名称',
-          trigger: 'blur'
-        },
-        {
-          min: 3,
-          max: 5,
-          message: '长度在 3 到 5 个字符',
-          trigger: 'blur'
-        }
-        ],
-        region: [{
-          required: true,
-          message: '请选择活动区域',
-          trigger: 'change'
-        }],
-        date1: [{
-          type: 'date',
-          required: true,
-          message: '请选择日期',
-          trigger: 'change'
-        }],
-        date2: [{
-          type: 'date',
-          required: true,
-          message: '请选择时间',
-          trigger: 'change'
-        }],
-        type: [{
-          type: 'array',
-          required: true,
-          message: '请至少选择一个活动性质',
-          trigger: 'change'
-        }],
-        resource: [{
-          required: true,
-          message: '请选择活动资源',
-          trigger: 'change'
-        }],
-        desc: [{
-          required: true,
-          message: '请填写活动形式',
-          trigger: 'blur'
-        }]
-      }
+      responseResult: []
     }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
-    resetForm (formName) {
-      this.$refs[formName].resetFields()
+    userLogin () {
+      this.$axios
+        .post('/user/login', {
+          userAccount: this.loginForm.userAccount,
+          userPwd: this.loginForm.userPwd
+          // rememberMe: this.loginForm.rememberMe
+        })
+        .then(successResponse => {
+          if (successResponse.data.errCode === 200) {
+            this.$router.replace({ path: '/user/home' })
+          } else if (successResponse.data.errCode === 401) {
+            this.$message({
+              showClose: true,
+              message: successResponse.data.errMessage,
+              type: 'warning'
+            })
+          } else if (successResponse.data.errCode === 402) {
+            this.$message({
+              showClose: true,
+              message: successResponse.data.errMessage,
+              type: 'warning'
+            })
+          } else if (successResponse.data.errCode === 403) {
+            this.$message({
+              showClose: true,
+              message: successResponse.data.errMessage,
+              type: 'error'
+            })
+          } else if (successResponse.data.errCode === 400) {
+            this.$message({
+              showClose: true,
+              message: successResponse.data.errMessage,
+              type: 'error'
+            })
+          }
+        })
+        .catch(failResponse => {
+        })
     }
   }
 }
 </script>
+
+<style scoped>
+#login-page {
+  background-position: center;
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  position: fixed;
+}
+body {
+  margin: 0px;
+}
+.login-container {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 90px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
+
+.login_title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+  color: #505458;
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  color: rgb(235, 14, 169);
+  font: bolder;
+}
+.down-btn .right-btn {
+  float: right;
+}
+.register {
+  left: 25%;
+  position: relative;
+}
+</style>
